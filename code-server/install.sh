cdf02c9a12a580d918f663dbe4551775ba870486
uvelyster <48861250+uvelyster@users.noreply.github.com>
2025-07-07 15:56:00 +0900

    Update install.sh

--- code-server/install.sh
+++ code-server/install.sh
@@ -4,5 +4,5 @@ curl -L https://code-server.dev/install.sh | sh
 mkdir -p ~/.config/code-server
 cp  config.yaml ~/.config/code-server/config.yaml 
 
-systemctl enable code-server@$UESR
-systemctl start code-server@$UESR
+systemctl enable code-server@$USER
+systemctl start code-server@$USER
