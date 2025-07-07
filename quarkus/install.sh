db37144be115046d60e6c9f54456274eaadb4b24
uvelyster <48861250+uvelyster@users.noreply.github.com>
2025-07-07 16:00:38 +0900

    Update install.sh

--- quarkus/install.sh
+++ quarkus/install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-curl -s "https://get.sdkman.io" | bash
-
 dnf install -y zip unzip
-
-sdk install java 24.0.1-graal
-sdk install quarkus
-sdk install maven
-
-
+curl -s "https://get.sdkman.io" | bash
 
